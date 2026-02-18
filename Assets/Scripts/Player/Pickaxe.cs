fa2138c1c2f539e877b6880f57f0103a00106065
Eric Lamoureux <ericandbailey@gmail.com>
2026-02-18 14:58:18 -0500

    Mining Overhaul

--- Assets/Scripts/Player/Pickaxe.cs
+++ Assets/Scripts/Player/Pickaxe.cs
@@ -1,115 +1,55 @@
-using System.Collections;
 using UnityEngine;
 using UnityEngine.InputSystem;
 
 public class Pickaxe : MonoBehaviour
 {
+    [Header("References")]
     [SerializeField] ParticleSystem rockParticles;
     [SerializeField] SphereCollider sphereCollider;
-    [SerializeField] float pickaxeDamage = 1f;
-    [SerializeField] float miningCooldown = 0.5f;
+    [SerializeField] LayerMask rockLayer;
     [SerializeField] Animator anim;
-    
-    const string MINING_ANIMATION = "Pickaxe_Mine";
-    const string MINING_IDLE = "Pickaxe_Idle";
 
-    bool isMining;
-    bool miningCRActive = false;
-    Rock currentRock;
+    [Header("Mining")]
+    [SerializeField] float pickaxeDamage = 1f;
 
-    void OnEnable()
+    const string MINING_BOOL = "IsMining";
+
+    void CheckForRock()
     {
-        Rock.OnRockDestroyed += RockDestroyed;
-    }
+        bool rockHit = false;
 
-    void OnDisable()
-    {
-        Rock.OnRockDestroyed -= RockDestroyed;
-    }
+        Collider[] hits = Physics.OverlapSphere(sphereCollider.transform.position, sphereCollider.radius, rockLayer);
 
-    void Update()
-    {
-        MiningController();
-    }
-
-    void OnTriggerEnter(Collider other)
-    {
-        if (other.CompareTag("Rock") && other.TryGetComponent(out Rock rock))
+        foreach (var hit in hits)
         {
-            currentRock = rock;
+            if (hit.TryGetComponent(out Rock rock))
+            {
+                rock.DamageRock(pickaxeDamage);
+                rockHit = true;
+            }
+        }
+
+        if (rockHit && rockParticles != null)
+        {
+            rockParticles.Play();
         }
     }
 
-    void OnTriggerExit(Collider other)
+    public void OnPickImpact()
     {
-        if (other.TryGetComponent(out Rock rock) && rock == currentRock)
-        {
-            currentRock = null;
-            StopMining();
-        }
-    }    
-
-    void MiningController()
-    {
-        if (isMining && currentRock != null)
-        {
-            StartMining();
-        }
-        else if (!isMining)
-        {
-            StopMining();
-        }
-    }
-
-    void StartMining()
-    {
-        if (currentRock == null || miningCRActive)
-            return;
-
-        anim.Play(MINING_ANIMATION, 0, 0f);
-        miningCRActive = true;
-        StartCoroutine(MineRock());
-    }
-
-    void StopMining()
-    {
-        anim.Play(MINING_IDLE);
-        StopCoroutine(nameof(MineRock));
-        miningCRActive = false;
-    }
-
-    void RockDestroyed(Rock rock)
-    {
-        if (currentRock == rock)
-        {
-            currentRock = null;
-            StopMining();
-        }
-    }
-
-    IEnumerator MineRock()
-    {
-        while (isMining && currentRock != null)
-        {
-            currentRock.DamageRock(pickaxeDamage);
-            if (rockParticles != null && !rockParticles.isPlaying) rockParticles.Play();
-            yield return new WaitForSeconds(miningCooldown);
-        }
-
-        miningCRActive = false;
+        CheckForRock();
     }
 
     public void Mine(InputAction.CallbackContext context)
     {
         if (context.performed) 
         {
-            //Debug.Log("Attempting to mine");
-            isMining = true;
+            anim.SetBool(MINING_BOOL, true);
         }
 
         if (context.canceled)
         {
-            isMining = false;
+            anim.SetBool(MINING_BOOL, false);
         }
     }
 }
@@ -119,4 +59,7 @@ public class Pickaxe : MonoBehaviour
 // Allow hold to mine - done
 // Add pickaxe model - done
 // Add basic animation - done
-// Run mine command with animation --- FUTURE GOAL
+// Run mine command with animation - done
+
+// Screen shake
+// different sounds for hitting/not mining rock
